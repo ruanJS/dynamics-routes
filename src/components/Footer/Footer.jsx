35045d67c2f8728510d39bafbc36ca4b89f94f89
bruno1098 <rm98470@fiap.com.br>
2023-10-16 20:11:33 -0300

    Inserindo dados no arquivo footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+export default function Rodape() {
+    return (
+      <footer>
+        <p><Link href="/">Voltar</Link></p>
+            <p>&copy;Todos os direitos reservados - 2023</p>
+      </footer>
+    )
+  }
+  
