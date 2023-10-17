a902c5f7ad720d6106c60e8f0b4df2bfc2eefe4c
ruanJS <dooug.14@hotmail.com>
2023-10-16 22:46:18 -0300

    adicionando conteudo do sapato

---
+++ src/app/produtos/sapato/page.jsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Sapato(){
+    return (
+        <div>
+            <h1>Sapato</h1>
+            <div>
+                <h4>tênis Air Jordan foi produzido para o ex-jogador de basquete do Hall of Fame Michael Jordan durante seu tempo com o Chicago Bulls no final de 1984 e lançado ao público em 1º de abril de 1985.</h4>
+                <figure>
+                  <Image src="/img/tenis-jordan.png" alt="Sapato" width={250} height={320}/>
+                </figure>
+            </div>
+        </div>
+      )
+
+}
