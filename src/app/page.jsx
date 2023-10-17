ec96ac93d2cade6c416f5da0bd3a3e060990115a
ruanJS <rm550231@fiap.com.br>
2023-10-17 14:25:37 -0300

    arrumando os cards

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,30 +1,73 @@
-import Image from 'next/image'
+import Image from "next/image";
 
-export default function Home(){
+export default function Home() {
+  return (
+    <>
+      <div className="home">
+        <h1>Home</h1>
+      </div>
 
-  return(
-    <div className='home'>
-       <h1>Home</h1>
-      <a href='/produtos/bone'>
-          <Image src="/img/bone-59fifty.png" alt="Boné" width={300} height={220}/>
-          <figcaption>Boné 59Fifty</figcaption>
-      </a>
-      <a href='/produtos/calca'>
-          <Image src="/img/calça-nike.png" alt="Calça" width={250} height={320}/>
-          <figcaption>Calça Nike</figcaption>
-      </a>
-      <a href='/produtos/camisa'>
-          <Image src="/img/camisa-jordan.png" alt="Boné" width={290} height={300}/>
-          <figcaption>Camisa Jordan</figcaption>
-      </a>
-      <a href='/produtos/oculos'>
-          <Image src="/img/oculos-cartier.png" alt="Óculos" width={390} height={220}/>
-          <figcaption>Óculos Cartier</figcaption>
-      </a>
-      <a href='/produtos/sapato'>
-          <Image src="/img/tenis-jordan.png" alt="sapato" width={350} height={460}/>
-          <figcaption>Tênis Jordan</figcaption>
-      </a>
-    </div>
-  )
+      <div className="cards-container">
+        <div className="card">
+          <a href="/produtos/bone">
+            <Image
+              src="/img/bone-59fifty.png"
+              alt="Boné"
+              width={300}
+              height={220}
+            />
+            <figcaption>Boné 59Fifty</figcaption>
+          </a>
+        </div>
+
+        <div className="card">
+          <a href="/produtos/calca">
+            <Image
+              src="/img/calça-nike.png"
+              alt="Calça"
+              width={250}
+              height={320}
+            />
+            <figcaption>Calça Nike</figcaption>
+          </a>
+        </div>
+
+        <div className="card">
+          <a href="/produtos/camisa">
+            <Image
+              src="/img/camisa-jordan.png"
+              alt="Camisa"
+              width={290}
+              height={300}
+            />
+            <figcaption>Camisa Jordan</figcaption>
+          </a>
+        </div>
+
+        <div className="card">
+          <a href="/produtos/oculos">
+            <Image
+              src="/img/oculos-cartier.png"
+              alt="Óculos"
+              width={390}
+              height={220}
+            />
+            <figcaption>Óculos Cartier</figcaption>
+          </a>
+        </div>
+
+        <div className="card">
+          <a href="/produtos/sapato">
+            <Image
+              src="/img/tenis-jordan.png"
+              alt="Tênis"
+              width={350}
+              height={350}
+            />
+            <figcaption>Tênis Jordan</figcaption>
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
