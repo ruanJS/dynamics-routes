532a1894e626511af9cbb4fd7540bdaf604e4194
ruanJS <rm550231@fiap.com.br>
2023-10-17 14:23:06 -0300

    fazendo modificações no footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,13 +4,13 @@ export default function Rodape() {
   return (
     <footer className="footer">
       <p>
-        <Link href="/produtos/1">Produto - 1</Link> -
-        <Link href="/produtos/2">Produto - 2</Link> -
-        <Link href="/produtos/3">Produto - 3</Link> -
-        <Link href="/produtos/4">Produto - 4</Link> -
-        <Link href="/produtos/5">Produto - 5</Link>
+        <Link href="/produtos/1">Produto - 1</Link> - 
+        <Link href="/produtos/2">Produto - 2</Link> - 
+        <Link href="/produtos/3">Produto - 3</Link> - 
+        <Link href="/produtos/4">Produto - 4</Link> - 
+        <Link href="/produtos/5">Produto - 5</Link> - 
       </p>
-      <p>&copy;Todos os direitos reservados - 2023</p>
+      <p>&copy;Todos os direitos reservados para FiapTech⚡ - 2023</p>
     </footer>
   );
 }
