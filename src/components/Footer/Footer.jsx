801c7688449a815d6ac4d5d21bb8fa7b3b7e4ab2
ruanJS <rm550231@fiap.com.br>
2023-10-17 15:10:22 -0300

    arrumando link nav no footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,11 +4,11 @@ export default function Rodape() {
   return (
     <footer className="footer">
       <p>
-        <Link href="/produtos/1">Produto - 1</Link> - 
-        <Link href="/produtos/2">Produto - 2</Link> - 
-        <Link href="/produtos/3">Produto - 3</Link> - 
-        <Link href="/produtos/4">Produto - 4</Link> - 
-        <Link href="/produtos/5">Produto - 5</Link> - 
+        <Link href="/produtos/1">Produto - 1</Link>  ㅤㅤ
+        <Link href="/produtos/2">Produto - 2</Link>  ㅤㅤ
+        <Link href="/produtos/3">Produto - 3</Link> ㅤ ㅤ
+        <Link href="/produtos/4">Produto - 4</Link> ㅤ ㅤ
+        <Link href="/produtos/5">Produto - 5</Link> ㅤㅤ
       </p>
       <p>&copy;Todos os direitos reservados para FiapTech⚡ - 2023</p>
     </footer>
